021543900f63a0146b5fb6e96973748e1202dbba
John Schmidley <tlg8n@outlook.com>
2019-09-30 19:14:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,9 @@ monopoly = {
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {},
-	    :rent_in_dollars => {}
+	    :rent_in_dollars => {
+	      :one_piece_owned => 25
+	    }
 	  },
 	  }
 end
