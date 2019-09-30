bdae327ae57157977acd35e1598d9343673b566e
John Schmidley <tlg8n@outlook.com>
2019-09-30 19:24:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ monopoly = {
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad =>{},
-	      :b_and_o_railroad =>{}
+	      :b_and_o_railroad =>{},
 	      :shortline => {}
 	    },
 	    :rent_in_dollars => {
